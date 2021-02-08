a29531ca0ff3a7d0b958db87451a6c345e8b5ceb
Parastoo <parastuughorbani@gmail.com>
2021-02-08 20:56:10 +0330

    Initial Commit

---
+++ UniSchedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniSchedule
+//
+//  Created by Parastoo on 2/8/21.
+//  Copyright © 2021 Parastoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
